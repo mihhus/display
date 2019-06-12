44388c5a321064ec6bd3a052774370c0aaadfb43
mihhus <gi33ti51@gmail.com>
2019-06-12 15:15:02 +0900

    Refer to issue for work in progress

--- syncgen.v
+++ syncgen.v
@@ -16,7 +16,7 @@ module syncgen(
     input               DCLK,           //ドットクロック
     input               DRST,           //リセット
     input       [1:0]   RESOL,          //解像度設定信号
-    input               VRSTART,        //表示開始
+    output              VRSTART,        //表示開始
     output  reg         DSP_HSYNC_X ,    //水平同期信号
     output  reg         DSP_VSYNC_X,    //垂直同期信号
     output  reg         DSP_preDE      //DSP_DEの１つ前の信号(1で画像データが有効)
@@ -27,41 +27,15 @@ module syncgen(
 reg [10:0]  HCNT;           //水平カウンタ
 reg [10:0]  VCNT;            //垂直カウンタ
 
-//ステートマシン
-parameter S_IDLE=2'b01, S_DISP=2'b10;
-
-reg     [1:0] CUR;  //ステートマシン
-reg     [1:0] NXT;  //ステート生成
-
-//CUR
-always @(posedge DCLK) begin
-    if(DRST) begin
-        CUR <= S_IDLE;
-    end
-    else begin
-        CUR <= NXT;
-    end
-end //CUR
-
-//NXT
-always @* begin
-    case(CUR)
-        S_IDLE: if(VRSTART) begin
-                    NXT <= S_DISP;
-                end
-        S_DISP: if (VCNT==VSC-1) begin
-                    NXT <= S_DISP;
-                end
-        default: NXT <= S_IDLE;
-    endcase
-end //NXT
+//VRSTART
+assign VRSTART = (VCNT==VFP+VPW+VBP) ? 1 : 0;
 
 //DSP_HSYNC_X
 always@(posedge DCLK) begin
     if(DRST==1'b1) begin
         DSP_HSYNC_X <= 11'h1;
     end
-    else if(CUR==S_DISP) begin
+    else begin
         if(HCNT == HFP-1) begin
             DSP_HSYNC_X <= 11'h0;
         end
@@ -76,7 +50,7 @@ always@(posedge DCLK) begin
     if(DRST==1'b1) begin
         DSP_VSYNC_X <= 11'h1;
     end
-    else if(CUR==S_DISP)begin
+    else begin
         if(VCNT == VFP & HCNT == HFP-1) begin
         //立下りをHSYNCと合わせる
             DSP_VSYNC_X <= 1'b0;
@@ -93,7 +67,7 @@ always@(posedge DCLK) begin
     if(DRST==1'b1) begin
         DSP_preDE <= 1'b0;
     end
-    else if(CUR==S_DISP)begin
+    else begin
         if((VCNT==VFP+VPW+VBP+VDO)|(HCNT==HFP+HPW+HBP+HDO-2)) begin
             DSP_preDE <= 1'b0;
         end
@@ -108,7 +82,7 @@ always@(posedge DCLK) begin
     if(DRST==1'b1) begin
         HCNT <= 1'b0;
     end
-    else if(CUR==S_DISP)begin
+    else begin
         if(HCNT == HSC-1) begin
             HCNT <= 1'b0;
         end
@@ -124,7 +98,7 @@ always@(posedge DCLK) begin
     if(DRST==1'b1) begin
         VCNT <= 1'b0;
     end
-    else if(CUR==S_DISP)begin
+    else begin
         if(HCNT == HSC-1'b1) begin
             if(VCNT == VSC-1'b1) begin
                 VCNT <= 1'b0;
