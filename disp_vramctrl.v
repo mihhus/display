5b6a73ca24c46eeea387c176788708431f5187c3
mihhus <gi33ti51@gmail.com>
2019-06-26 16:41:54 +0900

    change watch_dogs

--- disp_vramctrl.v
+++ disp_vramctrl.v
@@ -48,7 +48,7 @@ module disp_vramctrl
 parameter S_IDLE = 4'b0001, S_SETADDR = 4'b0010, S_READ = 4'b0100, S_WAIT = 4'b1000;
 
 //VGAの時の画素数/8(１トランザクションで送れる画素数)=必要なトランザクション数
-parameter watch_dogs = 16'h9600; //16'd38400
+parameter watch_dogs = 16'h12C0; //16'd4800
 
 //ARチャネルの送信側
 //ARADDR 8*32=256が1トランザクションなので
@@ -83,7 +83,7 @@ always @* begin
                         NXT <= S_SETADDR;
                     end
         S_READ: if(RLAST&RVALID) begin  //VRAMを読み出し、FIFOにつく
-                    if(COUNT==watch_dogs) begin//一画面分終了したらS_IDLEに戻る, カウンタが必要
+                    if(COUNT==watch_dogs-1) begin//一画面分終了したらS_IDLEに戻る, カウンタが必要
                         NXT <= S_IDLE;
                     end
                     else if(BUF_WREADY) begin   //バッファに余裕があればS_SETADDRに移動
@@ -118,7 +118,7 @@ always @(posedge ACLK) begin
     else if(CUR==S_SETADDR&ARREADY) begin
         COUNT <= COUNT + 1;
     end
-    else if(COUNT==watch_dogs&RLAST&RREADY) begin
+    else if(COUNT==watch_dogs-1&CUR==S_IDLE) begin
         COUNT <= 0;
     end
 end//COUNT
